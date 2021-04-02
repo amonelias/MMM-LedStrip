eb1906052e32f6384596ac4307ae6c9ecb1c1194
amonelias <81261363+amonelias@users.noreply.github.com>
2021-04-02 20:30:54 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -69,7 +69,6 @@ module.exports = NodeHelper.create({
     }
   },
 
-  // https://stackoverflow.com/questions/1573053/javascript-function-to-convert-color-names-to-hex-codes
   colorNameToHex: function(color){
     let colors = {"aliceblue":"f0f8ff","antiquewhite":"faebd7","aqua":"00ffff","aquamarine":"7fffd4","azure":"f0ffff",
     "beige":"f5f5dc","bisque":"ffe4c4","black":"000000","blanchedalmond":"ffebcd","blue":"0000ff","blueviolet":"8a2be2","brown":"a52a2a","burlywood":"deb887",
